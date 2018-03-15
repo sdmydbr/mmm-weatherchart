d0d8e61f7127da8da6bafb9d51ad4a1aa13eec59
Szech <Szech@WEYLAND>
2018-03-15 01:18:44 +0000

    apply custom svg colours
    - use simple regex for svg colour replacement
    - allow user to set custom colours in config

--- mmm-weatherchart.js
+++ mmm-weatherchart.js
@@ -11,13 +11,27 @@ Module.register("mmm-weatherchart", {
 		domain: "www.yr.no",
 		path: "/place/",
 		mmDirectory: "/home/pi/MagicMirror/", // not sure whether it is possible to ask MM for this path?
-		useSVG: false,
-		svgParameters: []
+		useSVG: true,
+        customiseSVG: true,    // change colours in hex values or "default" for no change
+        background_colour:    "#000000",
+        title_text_colour : "#f2f2f2",    // "Meteogram for...."
+        date_text_colour : "#f2f2f2",     // "Tuesday"
+        temperature_text_colour: "#f2f2f2", // vertical axis
+        rain_text_colour: "#f2f2f2",        // amount of rain 
+        below_zero_line_colour: "#74c4fe", 
+        above_zero_line_colour: "#ffdb48",    
+        gridline_colour: "#5c5c5c",
+        wind_direction_colour: "#ebebeb",
+        rain_colour: "#83d2fe",
+        snow_colour: "#ffffff",   
+        moon_colour_a: "#afb3b6", 
+        moon_colour_b: "#acafb3"
+		
 	},
 
 	// Define required scripts.
 	getScripts: function() {
-		return ["moment.js", "svg-modify.js", "del.js"];
+		return ["moment.js", "hashmap.js"];
 	},
 
 	getDom: function() {
@@ -45,42 +59,69 @@ Module.register("mmm-weatherchart", {
 		this.loaded = false;
 		this.scheduleUpdate(3); // wait some 3 secs and run initial update
 		this.updateTimer = null;
+		this.customColours = this.createCustomColourArray();
 	},
 
-   getSVGWeatherMap: function() {
-        var self = this;
-        var mapLocal = this.config.path + this.config.country + "/" + this.config.area + "/" + this.config.city + "/meteogram.svg";
-        var payload = {
-            domain: this.config.domain,
-            path: mapLocal,
-            mmDir: this.config.mmDirectory
-        };
-        self.sendSocketNotification("FETCH_MAP", payload);
-    },
-	
-	
 	
 	getWeatherMap: function() {
 		var self = this;
-		var mapLocal = this.config.path + this.config.country + "/" + this.config.area + "/" + this.config.city + "/meteogram.png";
+		var filetype = this.config.useSVG == true ? "svg" : "png"
+		var mapLocal = this.config.path + this.config.country + "/" + this.config.area + "/" + this.config.city + "/meteogram." + filetype;
 		var payload = {
 			domain: this.config.domain,
 			path: mapLocal,
-			mmDir: this.config.mmDirectory
+			mmDir: this.config.mmDirectory,
+			useSVG: this.config.useSVG,
+			customiseSVG: this.config.customiseSVG,
+			customColours: this.customColours
 		};
+        console.log("Downloading weather map from URL: " + payload.domain + payload.path);
+        console.log("Image type = " + filetype + " ; customiseSVG = " + this.config.customiseSVG);
+
 		self.sendSocketNotification("FETCH_MAP", payload);
 	},
 
 	socketNotificationReceived: function(notification, payload) {
+	    var self = this;
 		if (notification === "MAPPED"){
 			this.srcMap = payload;
 			if (typeof this.srcMap !== "undefined") {
-				this.loaded = true;
-				this.updateDom();
+		        this.loaded = true;
+		        this.updateDom();
 			}
 			this.scheduleUpdate();
 		}
 	},
+	
+	
+	
+	// return 2d array (map) of new colours to be set in the meteogram
+	// key = colour; value = replacement colour from config
+	createCustomColourArray: function() {
+	    var array = [
+	        ["fill:black", 'fill:' + this.config.wind_direction_colour],
+    	    ['#D7EFFA', this.config.background_colour],        // outer background
+    	    ['rect x="0" y="0" fill="white"', 'rect x="0" y="0" fill="' + this.config.background_colour + '"'],  // inner background
+    	    ["Arial;", "Arial; " + "fill: " + this.config.title_text_colour + ";"],
+    	    ["#000080", this.config.title_text_colour],
+    	    ["#212D2C", this.config.temperature_text_colour],
+    	    ["#EAEBE6", this.config.gridline_colour],
+    	    ["#8B918F", this.config.wind_direction_colour],
+    	    ["#505956", this.config.date_text_colour],
+    	    ["#F01C1C", this.config.above_zero_line_colour],
+    	    ["#0280D9", this.config.below_zero_line_colour],
+    	    ["#47c0e3", this.config.snow_colour],  
+    	    ["#0062bf", this.config.rain_colour],  // rain drop
+    	    ["#37BFE1", this.config.rain_colour],  // rain level
+    	    ['g id="logo-yr"', 'g id="logo-yr" fill="#231F20" '], // make yr logo visible
+    	    ['#686e73', this.config.moon_colour_a],
+    	    ['#6a7075', this.config.moon_colour_b],
+    	    
+	    ];
+	    
+	    return array;
+	},
+	
 
 	scheduleUpdate: function(delay) {
 		var nextLoad = this.config.updateInterval;
@@ -90,12 +131,7 @@ Module.register("mmm-weatherchart", {
 		var self = this;
 		clearTimeout(this.updateTimer);
 		this.updateTimer = setTimeout(function() {
-			if(this.useSVG){
-			    self.getSVGWeatherMap();
-			}
-			else {
-			    self.getWeatherMap();
-			}
+		    self.getWeatherMap();
 		}, nextLoad);
 	},
 });
--- node_helper.js
+++ node_helper.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var del = require('del');
 var request = require('request'); 
 var NodeHelper = require("node_helper");
+var HashMap = require("hashmap");
 
 module.exports = NodeHelper.create({
 
@@ -11,27 +12,81 @@ module.exports = NodeHelper.create({
 	},
 
 	socketNotificationReceived: function(notification, payload) {
-		console.log("Downloading weather map with signal: " + notification + " From URL: " + payload.domain + payload.path);
+		console.error("Downloading weather map with signal: " + notification + " From URL: " + payload.domain + payload.path);
+        var self = this;
 		if (notification === "FETCH_MAP"){
-			var self = this;
 			var options = {
 				host: payload.domain,
 				path: payload.path
 			};
+			var imgType = payload.useSVG ? '.svg' : '.png';
 			http.get(options, function (response) {
 				var pngFiles = payload.mmDir + 'modules/mmm-weatherchart/cache/*.png';
-				del([pngFiles]);
-				var cachedFile = new Date().getTime() + '.png';
+                var svgFiles = payload.mmDir + 'modules/mmm-weatherchart/cache/*.svg';
+				del([pngFiles, svgFiles]);
+				var cachedFile = new Date().getTime() + imgType;
 				var newImage = fs.createWriteStream(payload.mmDir + 'modules/mmm-weatherchart/cache/' + cachedFile);
-				var meteogram = '/modules/mmm-weatherchart/cache/' + cachedFile;
+				var imagePath = '/modules/mmm-weatherchart/cache/' + cachedFile;
 				response.on('data', function(chunk){
 					newImage.write(chunk);
 				});
 				response.on('end', function(){
 					newImage.end();
-					self.sendSocketNotification("MAPPED", meteogram);
+					if(payload.useSVG && payload.customiseSVG){
+				        console.log("imagePath = " + imagePath);
+
+					    var svgFilepath = payload.mmDir + imagePath.substring(1);
+					    var meteogram = self.readSVG(svgFilepath);
+
+                        var customColours = new HashMap(payload.customColours);
+					    self.customiseSVG(meteogram, customColours, svgFilepath);
+					}
+					
+				    self.sendSocketNotification("MAPPED", imagePath);
+					
 				});
 			});
 		}
+		
+		
 	},
+	
+	readSVG: function(svgFilepath){
+        var self = this;
+	    console.log(">> readSVG");
+
+	    console.log("svgFilepath = " + svgFilepath);
+	    var svgData = fs.readFileSync(svgFilepath,'utf8');
+
+	    return svgData;
+        console.log("<< readSVG");
+	},
+	
+   customiseSVG: function(meteogram, customColours, svgFilepath){
+       var self = this;
+       console.log(">> customiseSVG");
+      
+       console.log("iterating....");
+       customColours.forEach(function(value, key) {
+           console.log(key + ' ==> ' + value);
+
+           var reg = new RegExp(key,"g");   // not the safest way to do this, but #yolo
+           meteogram = meteogram.replace(reg, value);
+       });
+       
+       
+       console.log("writing file....");
+       fs.writeFile(svgFilepath, meteogram, 'utf-8', function(err) {
+           if(err) {
+               return console.log(err);
+           }
+
+           console.log("The file was saved!");
+       }); 
+       console.log("<< customiseSVG");
+   
+   
+   },
+	
+	
 });
